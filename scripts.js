ee283de839c7eb705a830b324826feb12e29016c
kauezind2 <kaue.23alves2022@gmail.com>
2024-03-15 08:23:31 -0300

    variavel nome

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
 
 function alertar(event){
-     alert("Voce clicou no botao!!!" + event);
+     alert("Voce clicou no botao!!!" + " " + nome.value);
 
      var numero = 6;
      var resultado = numero % 2;
